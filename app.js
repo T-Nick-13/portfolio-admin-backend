cacb04d7a59786842fae7da1831e92c69ab16608
Nikita Titov <netitov@gmail.com>
2022-07-19 17:43:18 +0300

    настройка cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const router = require('./routes/cards');
 const authRouter = require('./routes/auth');
 const userRouter = require('./routes/users');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 const helmet = require('helmet');
 const limiter = require('./utils/limiter');
 const path = require('path');
@@ -20,7 +20,7 @@ mongoose.connect('mongodb://localhost:27017/portfolio', {
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.use(cors());
+//app.use(cors());
 app.use(limiter);
 app.use(helmet());
 app.use(bodyParser.json());
