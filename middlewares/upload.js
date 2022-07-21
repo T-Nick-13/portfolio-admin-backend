b73df0dad8bfeafa1971fb7a88e2b9e68323bd43
Nikita Titov <netitov@gmail.com>
2022-07-21 13:44:23 +0300

    скорректирован путь сохранения картинок; добавлена возможность сохранять формат svg

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -10,7 +10,9 @@ const storage = multer.diskStorage({
 });  
 
 const fileFilter = (req, file, cb) => {
-    if((file.mimetype).includes('jpeg') || (file.mimetype).includes('png') || (file.mimetype).includes('jpg')){
+    if((file.mimetype).includes('jpeg') || (file.mimetype).includes('png') || (file.mimetype).includes('jpg')
+    || (file.mimetype).includes('svg')
+    ){
         cb(null, true);
     } else{
         cb(null, false);
