e9970dddbea35ed04f08700757e2d11ac49cb6f9
Nikita Titov <netitov@gmail.com>
2022-09-17 17:46:39 +0300

    скорректированы параметры cors

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const helmet = require('helmet');
 const limiter = require('./utils/limiter');
 const path = require('path');
 
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://stafeeva.site'
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Accept'],
+  credentials: true,
+};
+
 const { PORT = 3001 } = process.env;
 const app = express(); 
 
@@ -18,9 +31,10 @@ mongoose.connect('mongodb://localhost:27017/portfolio', {
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.use(cors());
+//app.use(cors());
 app.use(limiter);
 app.use(helmet());
+app.use('*', cors(options));
 app.use(bodyParser.json());
 
 app.use('/', router);
